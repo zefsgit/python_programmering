795fd8023253c679d44a41b851b253aed0dad947
zefsgit <43710848+zefsgit@users.noreply.github.com>
2022-01-27 09:31:29 +0100

    old files

---
+++ Lecture09/animal/main.py
@@ -0,0 +1,13 @@
+from animal import Animal
+
+def main():
+    dog = Dog()
+    dog.make_noise()
+    print('Energy', dog.energy)
+    dog.play
+    print('Energy', dog.energy)
+
+
+if __name__ == "__main__":
+    main()
+
